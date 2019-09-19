115710ff6f6ee1edb05b8d727b55e4ac0ef0a5ee
Andresa Martins <contact@andresa.dev>
2019-09-19 01:52:49 -0300

    Add Questions Grid

---
+++ Controller/Adminhtml/Question/Index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Question Index Controller
+ *
+ * @category   Prestafy
+ * @package    Prestafy_Faq
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Prestafy\Faq\Controller\Adminhtml\Question;
+
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        /** @var Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        return $result;
+    }
+}
