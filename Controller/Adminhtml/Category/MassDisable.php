0c6d112ecbc87c742fab283145e8ec333deba3a7
Andresa Martins <contact@andresa.dev>
2019-09-19 00:53:25 -0300

    Category Grid Mass Actions

---
+++ Controller/Adminhtml/Category/MassDisable.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Mass Disable Action for Category Grid
+ *
+ * @category   Prestafy
+ * @package    Prestafy_Faq
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Prestafy\Faq\Controller\Adminhtml\Category;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Ui\Component\MassAction\Filter;
+use Prestafy\Faq\Model\ResourceModel\Category\CollectionFactory;
+
+class MassDisable extends \Magento\Backend\App\Action
+{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        parent::__construct($context);
+
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Execute Mass Disable Action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection as $item) {
+            $item->setStatus(0);
+            $item->save();
+        }
+
+        $this->messageManager->addSuccessMessage(__('A total of %1 categories have been disabled.', $collectionSize));
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
---
+++ Controller/Adminhtml/Category/Massdelete.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Mass Delete Action for Category Grid
+ *
+ * @category   Prestafy
+ * @package    Prestafy_Faq
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Prestafy\Faq\Controller\Adminhtml\Category;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Ui\Component\MassAction\Filter;
+use Prestafy\Faq\Model\ResourceModel\Category\CollectionFactory;
+
+class Massdelete extends \Magento\Backend\App\Action
+{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        parent::__construct($context);
+
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+    }
+
+    /**
+     * Execute Mass Delete Action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+        foreach ($collection as $item) {
+            $item->delete();
+        }
+
+        $this->messageManager->addSuccessMessage(__('A total of %1 categories have been deleted.', $collectionSize));
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
