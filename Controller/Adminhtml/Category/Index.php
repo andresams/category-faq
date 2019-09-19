7d4b65bf2870710bc9bfb209e50e8a9a5b1bb6ff
Andresa Martins <contact@andresa.dev>
2019-09-18 21:29:58 -0300

    Create module route and Category Index Controller

---
+++ Controller/Adminhtml/Category/Index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Category Index Controller
+ *
+ * @category   Prestafy
+ * @package    Prestafy_Faq
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Prestafy\Faq\Controller\Adminhtml\Category;
+
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * Execute controller action
+     *
+     * @return ResponseInterface|Raw|ResultInterface
+     */
+    public function execute()
+    {
+        /** @var Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+        return $result;
+    }
+}
