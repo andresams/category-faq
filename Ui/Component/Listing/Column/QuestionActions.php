e5e0e3441698e65947bb5cc997af86e05f507623
Andresa Martins <contact@andresa.dev>
2019-09-21 00:52:11 -0300

    Added action to the categories grid

---
+++ Ui/Component/Listing/Column/QuestionActions.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Add Actions to the Questions Grid
+ *
+ * @category   Prestafy
+ * @package    Prestafy_Faq
+ * @author     Andresa Martins <contact@andresa.dev>
+ * @copyright  Copyright (c) 2019 Prestafy eCommerce Solutions (https://www.prestafy.com.br)
+ * @license    http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Prestafy\Faq\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+class QuestionActions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Add Edit Question link to Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'faq/question/edit',
+                        ['id' => $item['id']]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
